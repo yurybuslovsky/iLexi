d02c8d7db52448ea15ca4f1f21bfaa23cc032a65
Yury Buslovsky <yury.buslovsky@gmail.com>
2020-08-31 18:11:01 +0500

    Add core framework

---
+++ Core/Core/Core.h
@@ -0,0 +1,19 @@
+//
+//  Core.h
+//  Core
+//
+//  Created by Yury Buslovsky on 31.08.2020.
+//  Copyright © 2020 Napoleon IT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Core.
+FOUNDATION_EXPORT double CoreVersionNumber;
+
+//! Project version string for Core.
+FOUNDATION_EXPORT const unsigned char CoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Core/PublicHeader.h>
+
+
--- Podfile
+++ Podfile
@@ -4,12 +4,22 @@ use_frameworks!
 
 workspace 'iLexi'
 
-def app_pods
-  pod 'RxSwift', '~> 5'
-  pod 'RxCocoa', '~> 5'
+def core_pods
+    # Rx
+    pod 'RxSwift', '~> 5'
+    pod 'RxCocoa', '~> 5'
+end
+
+def iLexi_pods
+    core_pods
+end
+
+target 'Core' do
+  project 'Core/Core.project'
+  core_pods
 end
 
 target 'iLexi' do
   project './iLexi.project'
-  app_pods
+  iLexi_pods
 end
